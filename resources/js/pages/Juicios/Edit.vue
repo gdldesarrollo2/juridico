eef25765422e138fa65a848ee9a8b932716d7e01
veronicavel91 <veronica.vel91@gmail.com>
2025-09-23 16:32:41 -0600

    edicion de juicios

--- resources/js/pages/Juicios/Edit.vue
+++ resources/js/pages/Juicios/Edit.vue
@@ -2,12 +2,13 @@
 import { Link } from '@inertiajs/vue3'
 import FormJuicio from './components/FormJuicio.vue'
 
-// Props que manda el controlador create()
 const props = defineProps<{
+  juicio: any
   clientes: {id:number; nombre:string}[]
   autoridades: {id:number; nombre:string}[]
   abogados: {id:number; nombre:string}[]
   etiquetas: {id:number; nombre:string}[]
+  etiquetasSeleccionadas: number[]
 }>()
 </script>
 
@@ -15,17 +16,18 @@ const props = defineProps<{
   <TopNavLayout />
   <div class="bg-white rounded-xl p-5 shadow space-y-4">
     <div class="flex items-center gap-2">
-      <h1 class="text-2xl font-semibold">Nuevo Juicio</h1>
+      <h1 class="text-2xl font-semibold">Editar Juicio #{{ props.juicio.id }}</h1>
       <Link :href="route('juicios.index')" class="ml-auto text-indigo-600 hover:underline">Volver</Link>
     </div>
 
     <FormJuicio
-      mode="create"
-      :initial="{ }"
+      mode="edit"
+      :initial="props.juicio"
       :clientes="props.clientes"
       :autoridades="props.autoridades"
       :abogados="props.abogados"
       :etiquetas="props.etiquetas"
+      :etiquetasSeleccionadas="props.etiquetasSeleccionadas"
     />
   </div>
 </template>
